551cb2ff7d50ac289161dd159e2cb3f1bb365c42
DominiqueDockal <dominique.dockal@gmail.com>
2025-05-15 16:09:51 +0200

    keyboard, script added

---
+++ script.js
@@ -0,0 +1,33 @@
+function init() {
+    canvas = document.getElementById('canvas');
+}
+
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowLeft':  keyboard.LEFT = true; break;
+        case 'ArrowRight': keyboard.RIGHT = true; break;
+        case 'ArrowUp':    keyboard.UP = true; break;
+        case 'ArrowDown':  keyboard.DOWN = true; break;
+        case 'Space':      keyboard.SPACE = true; break;
+    }
+    if (event.key === 'd') keyboard.D = true;
+});
+
+document.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'ArrowLeft':  keyboard.LEFT = false; break;
+        case 'ArrowRight': keyboard.RIGHT = false; break;
+        case 'ArrowUp':    keyboard.UP = false; break;
+        case 'ArrowDown':  keyboard.DOWN = false; break;
+        case 'Space':      keyboard.SPACE = false; break;
+    }
+    if (event.key === 'd') keyboard.D = false;
+});
+
+
+
+//  für die mobile ansicht Buttons einfügt, die hier ebenfalls abgefragt werden müssen über Element by ID
+
+function fullscreen(){
+    
+}
