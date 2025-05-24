517a69dcca218fcfe26f7c152ec6add735b2e1ef
DominiqueDockal <dominique.dockal@gmail.com>
2025-05-24 19:21:31 +0200

    level initalisation fixed

--- classes/game.class.js
+++ classes/game.class.js
@@ -1,16 +1,19 @@
 class Game {
     collectedCoins = 0;
     currentLevel;
+    currentLevelNumber = 0;
     level1Data = [3, 2, 15, 15, 30]; // Einfach - Viele Ressourcen, wenige Gegner
     level2Data = [5, 4, 12, 12, 15]; // Mittel - Ausgewogene Verteilung  
     level3Data = [7, 6, 8, 8, 10];   // Schwer - Wenige Ressourcen, viele Gegner
+    allLevels =[this.level1Data,this.level2Data, this.level3Data];
 
     constructor(){
-        this.initializeLevel(level1Data);   
+        this.initializeLevel();   
     }
 
-    initializeLevel(levelData){
-        this.currentLevel = new level(levelData, this.collectedCoins);
+    // für erweiterbare Logik
+    initializeLevel(){
+        this.currentLevel = new level(this.allLevels[this.currentLevelNumber], this.collectedCoins);
     }
     
 }
